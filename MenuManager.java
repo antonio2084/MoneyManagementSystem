9f62221f3d5ca030b316e22c9c67fa37b82490ea
antonio2084 <127909568+antonio2084@users.noreply.github.com>
2023-03-27 18:55:16 +0900

    Update MenuManager.java

--- MenuManager.java
+++ MenuManager.java
@@ -1,11 +1,41 @@
+import java.util.Scanner;
+
 
 public class MenuManager {
 	public static void main(String[] args) {
+		int num = 0;
+		Scanner input = new Scanner(System.in);
+		while(num != 6){
 		System.out.println("1.Add Money");
 		System.out.println("2.Delete Money");
 		System.out.println("3.Edit Money");
 		System.out.println("4.View Money");
 		System.out.println("5.Show a menu");
 		System.out.println("6.Exit");
+		System.out.println("Select one number between 1~6 : ");
+		num = input.nextInt();
+		if(num ==1){
+			System.out.print("Input money: ");
+			int moneyin = input.nextInt();
+			System.out.print("How to use: ");
+			String HowToUse = input.next();
+		}
+		else if(num ==2){
+			System.out.print("Input money: ");
+			int moneyin = input.nextInt();
+		}
+		else if(num ==3){
+			System.out.print("Input money: ");
+			int moneyin = input.nextInt();
+		}
+		else if(num ==4){
+			System.out.print("Input money: ");
+			int moneyin = input.nextInt();
+		}
+		else if(num ==5){
+			System.out.print("Input money: ");
+			int moneyin = input.nextInt();
+		}
+		}
 	}
 }
